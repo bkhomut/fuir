9693007a0581028fa45d9594d67d0f26e0630699
Boris <bkhomut2013@kellogg.northwestern.edu>
2014-01-04 14:01:47 -0500

    generate app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fuir::Application.config.secret_token = 'dab4664e5ac4d448067852246a9bb1fde56898de359b0ffc28eda7b08ff29a9ca76b354ca461f6b3122e4b1968bdd56099bc64992e9b038df04c92a9da906120'
